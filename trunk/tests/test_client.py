6c1749dd911418c9a9295e157910845904b069ce
dsteinmann@acm.org <dsteinmann@acm.org@19ca2394-1d08-11de-b6aa-3535bc3264a2>
2009-07-19 21:54:15 +0000

    Tests in Module aufgeteilt: Divide and Conquer

--- trunk/tests/test_client.py
+++ trunk/tests/test_client.py
@@ -6,73 +6,19 @@ import unittest
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-import views
+from sasse.views import wettkaempfe_get
+from sasse.views import wettkaempfe_by_year
+from sasse.views import wettkaempfe_add
+from sasse.views import wettkampf_get
+from sasse.views import wettkampf_update
+from sasse.views import wettkampf_delete_confirm
 
-from forms import get_kategorie
-from forms import get_startkategorie
-from forms import WettkampfForm
-
-from models import Disziplinart
-from models import Kategorie
-from models import Postenart
-from models import Wettkampf
-from models import Mitglied
-from models import Sektion
-
-from fields import UnicodeSlugField
-
-
-class WettkampfFormTest(TestCase):
-    def setUp(self):
-        # Fülle Datenbank mit einigen älteren Wettkämpfen
-        Wettkampf.objects.create(name="Test-Cup", von="2003-04-04")
-        Wettkampf.objects.create(name="Test-Cup", von="2005-04-04")
-        Wettkampf.objects.create(name="Test-Cup", von="2007-04-04")
-        # Gültiger Wettkampf
-        self.form = WettkampfForm(data={
-            'name': 'Test-Cup',
-            'zusatz': 'Irgendwo',
-            'von': '2009-04-10',
-            'bis': '2009-04-11',
-            })
-
-    def test_valid(self):
-        self.failUnless(self.form.is_valid(), self.form.errors)
-
-    def test_invalid_name(self):
-        self.form.data['name'] = u'No Spaces'
-        self.failUnless(self.form.errors.has_key('name'))
-        expected = UnicodeSlugField.default_error_messages['invalid']
-        self.failUnless(expected in self.form.errors['name'])
-
-    def test_earlier_bis_than_von(self):
-        self.form.data['bis'] = '2009-04-09'
-        expected = u"Von muss älter als bis sein"
-        self.failUnless(expected in self.form.non_field_errors())
-
-    def test_duplicate_name_for_new_wettkampf(self):
-        self.form.data['von'] = '2007-01-01'
-        expected = u"Der Name 'Test-Cup' ist im Jahr '2007' bereits vergeben"
-        self.failUnless(expected in self.form.non_field_errors())
-
-    def test_duplicate_name_for_existing_wettkampf(self):
-        w2009 = self.form.save()
-        self.form = WettkampfForm(data={
-            'name': 'Test-Cup',
-            'zusatz': 'Ein anderer Zusatz',
-            'von': '2007-01-01'
-            }, instance=w2009)
-        expected = u"Der Name 'Test-Cup' ist im Jahr '2007' bereits vergeben"
-        self.failUnless(expected in self.form.non_field_errors())
-
-    def test_update_existing_wettkampf(self):
-        w2009 = self.form.save()
-        self.form = WettkampfForm(data={
-            'name': w2009.name,
-            'zusatz': 'Ein anderer Zusatz',
-            'von': str(w2009.von),
-            }, instance=w2009)
-        self.failUnless(self.form.is_valid(), self.form.errors)
+from sasse.models import Disziplinart
+from sasse.models import Kategorie
+from sasse.models import Postenart
+from sasse.models import Wettkampf
+from sasse.models import Mitglied
+from sasse.models import Sektion
 
 
 class WettkampfPageTest(TestCase):
@@ -95,21 +41,21 @@ class WettkampfPageTest(TestCase):
                 )
 
     def test_list(self):
-        listURL = reverse(views.wettkaempfe_get)
+        listURL = reverse(wettkaempfe_get)
         response = self.client.get(listURL)
         self.failUnlessEqual(response.status_code, 200)
         self.assertContains(response, 'Basis-Cup')
         self.failUnlessEqual(len(response.context[-1]['wettkaempfe']), 3)
 
     def test_list_by_year(self):
-        listURL = reverse(views.wettkaempfe_by_year, args=['2008'])
+        listURL = reverse(wettkaempfe_by_year, args=['2008'])
         response = self.client.get(listURL)
         self.failUnlessEqual(response.status_code, 200)
         self.failUnlessEqual(response.context[-1]['year'], '2008')
         self.failUnlessEqual(len(response.context[-1]['wettkaempfe']), 2)
 
     def test_add(self):
-        addURL = reverse(views.wettkaempfe_add)
+        addURL = reverse(wettkaempfe_add)
         response = self.client.get(addURL)
         self.failUnlessEqual(response.status_code, 200)
         response = self.client.post(addURL, {})
@@ -119,11 +65,11 @@ class WettkampfPageTest(TestCase):
             'zusatz': 'Bremgarten',
             'von': '2009-05-20',
             })
-        getURL = reverse(views.wettkampf_get, args=['2009', 'Test-Cup'])
+        getURL = reverse(wettkampf_get, args=['2009', 'Test-Cup'])
         self.assertRedirects(response, getURL)
 
     def test_update(self):
-        updateURL = reverse(views.wettkampf_update, args=['2009', 'Basis-Cup'])
+        updateURL = reverse(wettkampf_update, args=['2009', 'Basis-Cup'])
         response = self.client.get(updateURL)
         self.failUnlessEqual(response.status_code, 200)
         self.assertContains(response, 'Basis-Cup')
@@ -134,16 +80,16 @@ class WettkampfPageTest(TestCase):
             'zusatz': 'Ein anderer Zusatz',
             'von': '2008-05-20',
             })
-        getURL = reverse(views.wettkampf_get, args=['2008', 'New-Cup'])
+        getURL = reverse(wettkampf_get, args=['2008', 'New-Cup'])
         self.assertRedirects(response, getURL)
 
     def test_delete(self):
-        deleteURL = reverse(views.wettkampf_delete_confirm,
+        deleteURL = reverse(wettkampf_delete_confirm,
                 args=['2009', 'Basis-Cup'])
         response = self.client.get(deleteURL)
         self.failUnlessEqual(response.status_code, 200)
         response = self.client.post(deleteURL)
-        listURL = reverse(views.wettkaempfe_get)
+        listURL = reverse(wettkaempfe_get)
         self.assertRedirects(response, listURL)
         response = self.client.get(listURL)
         self.assertNotContains(response, 'Basis-Cup')
@@ -313,86 +259,3 @@ class EinzelfahrenStartlistePageTest(TestCase):
         self.assertContains(response, 'Steinmann')
 
 #TODO: TeilneherSchiffeinzelPageTest(unittest.TestCase):
-
-class get_kategorie_Test(TestCase):
-    def setUp(self):
-        self.jahr = 2009
-        self.kat_I = Kategorie.objects.get(name='I')
-        self.kat_II = Kategorie.objects.get(name='II')
-        self.kat_III = Kategorie.objects.get(name='III')
-        self.kat_C = Kategorie.objects.get(name='C')
-        self.kat_D = Kategorie.objects.get(name='D')
-        self.kat_F = Kategorie.objects.get(name='F')
-        bremgarten = Sektion.objects.create(name="Bremgarten")
-        self.mann_C = Mitglied.objects.create(
-                name="Steinmann", vorname="Daniel", geschlecht="m",
-                geburtsdatum=datetime.date(1967, 4, 30),
-                sektion=bremgarten)
-        self.mann_D = Mitglied.objects.create(
-                name="Wendel", vorname="René", geschlecht="m",
-                geburtsdatum=datetime.date(1958, 1, 1),
-                sektion=bremgarten)
-        self.frau_F = Mitglied.objects.create(
-                name="Honegger", vorname="Patricia", geschlecht="f",
-                geburtsdatum=datetime.date(1977, 1, 1),
-                sektion=bremgarten)
-        self.frau_II = Mitglied.objects.create(
-                name="Leemann", vorname="Sarah", geschlecht="f",
-                geburtsdatum=datetime.date(1993, 1, 1),
-                sektion=bremgarten)
-
-    def testFrauNichtInKatF(self):
-        self.assertEquals(self.kat_II, get_kategorie(self.jahr, self.frau_II))
-
-    def testFrau(self):
-        self.assertEquals(self.kat_F, get_kategorie(self.jahr, self.frau_F))
-
-    def testMannKatC(self):
-        self.assertEquals(self.kat_C, get_kategorie(self.jahr, self.mann_C))
-
-    def testMannKatD(self):
-        self.assertEquals(self.kat_D, get_kategorie(self.jahr, self.mann_D))
-
-
-class get_startkategorie_Test(unittest.TestCase):
-    def setUp(self):
-        self.kat_I = Kategorie.objects.get(name='I')
-        self.kat_II = Kategorie.objects.get(name='II')
-        self.kat_III = Kategorie.objects.get(name='III')
-        self.kat_C = Kategorie.objects.get(name='C')
-        self.kat_D = Kategorie.objects.get(name='D')
-        self.kat_F = Kategorie.objects.get(name='F')
-
-    def _assert_kat(self, expected, a, b):
-        self.assertEquals(expected, get_startkategorie(a, b))
-        self.assertEquals(expected, get_startkategorie(b, a))
-
-    def testGleicheKategorie(self):
-        for k in Kategorie.objects.all():
-            self.assertEquals(k, get_startkategorie(k, k))
-
-    def testKatIIandI(self):
-        self._assert_kat(self.kat_II, self.kat_I, self.kat_II)
-
-    def testKatIIIandI(self):
-        self._assert_kat(self.kat_III, self.kat_I, self.kat_III)
-
-    def testKatIIIandII(self):
-        self._assert_kat(self.kat_III, self.kat_II, self.kat_III)
-
-    def testKatCandIII(self):
-        self._assert_kat(self.kat_C, self.kat_C, self.kat_III)
-
-    def testKatCandD(self):
-        self._assert_kat(self.kat_C, self.kat_C, self.kat_D)
-
-    def testKatCandF(self):
-        self._assert_kat(self.kat_C, self.kat_C, self.kat_F)
-
-    def testKatDandF(self):
-        self._assert_kat(self.kat_C, self.kat_D, self.kat_F)
-
-    def testUnbekannteKombination(self):
-        self._assert_kat(None, self.kat_II, self.kat_C)
-        self._assert_kat(None, self.kat_I, self.kat_D)
-        self._assert_kat(None, self.kat_I, self.kat_F)
---
+++ trunk/tests/test_forms.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import unittest
+
+from django.test import TestCase
+
+from sasse.forms import get_kategorie
+from sasse.forms import get_startkategorie
+from sasse.forms import WettkampfForm
+
+from sasse.models import Kategorie
+from sasse.models import Wettkampf
+from sasse.models import Mitglied
+from sasse.models import Sektion
+
+from sasse.fields import UnicodeSlugField
+
+
+class WettkampfFormTest(TestCase):
+    def setUp(self):
+        # Fülle Datenbank mit einigen älteren Wettkämpfen
+        Wettkampf.objects.create(name="Test-Cup", von="2003-04-04")
+        Wettkampf.objects.create(name="Test-Cup", von="2005-04-04")
+        Wettkampf.objects.create(name="Test-Cup", von="2007-04-04")
+        # Gültiger Wettkampf
+        self.form = WettkampfForm(data={
+            'name': 'Test-Cup',
+            'zusatz': 'Irgendwo',
+            'von': '2009-04-10',
+            'bis': '2009-04-11',
+            })
+
+    def test_valid(self):
+        self.failUnless(self.form.is_valid(), self.form.errors)
+
+    def test_invalid_name(self):
+        self.form.data['name'] = u'No Spaces'
+        self.failUnless(self.form.errors.has_key('name'))
+        expected = UnicodeSlugField.default_error_messages['invalid']
+        self.failUnless(expected in self.form.errors['name'])
+
+    def test_earlier_bis_than_von(self):
+        self.form.data['bis'] = '2009-04-09'
+        expected = u"Von muss älter als bis sein"
+        self.failUnless(expected in self.form.non_field_errors())
+
+    def test_duplicate_name_for_new_wettkampf(self):
+        self.form.data['von'] = '2007-01-01'
+        expected = u"Der Name 'Test-Cup' ist im Jahr '2007' bereits vergeben"
+        self.failUnless(expected in self.form.non_field_errors())
+
+    def test_duplicate_name_for_existing_wettkampf(self):
+        w2009 = self.form.save()
+        self.form = WettkampfForm(data={
+            'name': 'Test-Cup',
+            'zusatz': 'Ein anderer Zusatz',
+            'von': '2007-01-01'
+            }, instance=w2009)
+        expected = u"Der Name 'Test-Cup' ist im Jahr '2007' bereits vergeben"
+        self.failUnless(expected in self.form.non_field_errors())
+
+    def test_update_existing_wettkampf(self):
+        w2009 = self.form.save()
+        self.form = WettkampfForm(data={
+            'name': w2009.name,
+            'zusatz': 'Ein anderer Zusatz',
+            'von': str(w2009.von),
+            }, instance=w2009)
+        self.failUnless(self.form.is_valid(), self.form.errors)
+
+
+class get_kategorie_Test(TestCase):
+    def setUp(self):
+        self.jahr = 2009
+        self.kat_I = Kategorie.objects.get(name='I')
+        self.kat_II = Kategorie.objects.get(name='II')
+        self.kat_III = Kategorie.objects.get(name='III')
+        self.kat_C = Kategorie.objects.get(name='C')
+        self.kat_D = Kategorie.objects.get(name='D')
+        self.kat_F = Kategorie.objects.get(name='F')
+        bremgarten = Sektion.objects.create(name="Bremgarten")
+        self.mann_C = Mitglied.objects.create(
+                name="Steinmann", vorname="Daniel", geschlecht="m",
+                geburtsdatum=datetime.date(1967, 4, 30),
+                sektion=bremgarten)
+        self.mann_D = Mitglied.objects.create(
+                name="Wendel", vorname="René", geschlecht="m",
+                geburtsdatum=datetime.date(1958, 1, 1),
+                sektion=bremgarten)
+        self.frau_F = Mitglied.objects.create(
+                name="Honegger", vorname="Patricia", geschlecht="f",
+                geburtsdatum=datetime.date(1977, 1, 1),
+                sektion=bremgarten)
+        self.frau_II = Mitglied.objects.create(
+                name="Leemann", vorname="Sarah", geschlecht="f",
+                geburtsdatum=datetime.date(1993, 1, 1),
+                sektion=bremgarten)
+
+    def testFrauNichtInKatF(self):
+        self.assertEquals(self.kat_II, get_kategorie(self.jahr, self.frau_II))
+
+    def testFrau(self):
+        self.assertEquals(self.kat_F, get_kategorie(self.jahr, self.frau_F))
+
+    def testMannKatC(self):
+        self.assertEquals(self.kat_C, get_kategorie(self.jahr, self.mann_C))
+
+    def testMannKatD(self):
+        self.assertEquals(self.kat_D, get_kategorie(self.jahr, self.mann_D))
+
+
+class get_startkategorie_Test(unittest.TestCase):
+    def setUp(self):
+        self.kat_I = Kategorie.objects.get(name='I')
+        self.kat_II = Kategorie.objects.get(name='II')
+        self.kat_III = Kategorie.objects.get(name='III')
+        self.kat_C = Kategorie.objects.get(name='C')
+        self.kat_D = Kategorie.objects.get(name='D')
+        self.kat_F = Kategorie.objects.get(name='F')
+
+    def _assert_kat(self, expected, a, b):
+        self.assertEquals(expected, get_startkategorie(a, b))
+        self.assertEquals(expected, get_startkategorie(b, a))
+
+    def testGleicheKategorie(self):
+        for k in Kategorie.objects.all():
+            self.assertEquals(k, get_startkategorie(k, k))
+
+    def testKatIIandI(self):
+        self._assert_kat(self.kat_II, self.kat_I, self.kat_II)
+
+    def testKatIIIandI(self):
+        self._assert_kat(self.kat_III, self.kat_I, self.kat_III)
+
+    def testKatIIIandII(self):
+        self._assert_kat(self.kat_III, self.kat_II, self.kat_III)
+
+    def testKatCandIII(self):
+        self._assert_kat(self.kat_C, self.kat_C, self.kat_III)
+
+    def testKatCandD(self):
+        self._assert_kat(self.kat_C, self.kat_C, self.kat_D)
+
+    def testKatCandF(self):
+        self._assert_kat(self.kat_C, self.kat_C, self.kat_F)
+
+    def testKatDandF(self):
+        self._assert_kat(self.kat_C, self.kat_D, self.kat_F)
+
+    def testUnbekannteKombination(self):
+        self._assert_kat(None, self.kat_II, self.kat_C)
+        self._assert_kat(None, self.kat_I, self.kat_D)
+        self._assert_kat(None, self.kat_I, self.kat_F)
