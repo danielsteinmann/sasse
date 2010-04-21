ae7a4962faa57768864a83217d37050928f79906
dsteinmann@acm.org <dsteinmann@acm.org@19ca2394-1d08-11de-b6aa-3535bc3264a2>
2010-04-21 20:36:41 +0000

    Bessere Admin Seite fuer Mitglied Objekte.

--- trunk/admin.py
+++ trunk/admin.py
@@ -5,7 +5,13 @@ from models import Sektion
 from models import Postenart
 from models import Bewertungsart
 
-admin.site.register(Mitglied)
+class MitgliedAdmin(admin.ModelAdmin):
+    list_display = ('name', 'vorname', 'sektion', 'geburtsdatum', 'nummer',)
+    search_fields = ('name', 'vorname', 'sektion__name',)
+    actions_on_top = False
+    actions_on_bottom = True
+
+admin.site.register(Mitglied, MitgliedAdmin)
 admin.site.register(Sektion)
 admin.site.register(Postenart)
 admin.site.register(Bewertungsart)
