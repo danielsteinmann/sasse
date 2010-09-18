701e2a5cb449499cbfa56c730b789469fabc7ad3
dsteinmann@acm.org <dsteinmann@acm.org@19ca2394-1d08-11de-b6aa-3535bc3264a2>
2010-09-18 21:53:03 +0000

    Mit Inline Forms Admin Seite verbessert

--- trunk/admin.py
+++ trunk/admin.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
 from models import Mitglied
 from models import Sektion
+from models import Disziplinart
 from models import Postenart
 from models import Bewertungsart
 
@@ -11,7 +13,26 @@ class MitgliedAdmin(admin.ModelAdmin):
     actions_on_top = False
     actions_on_bottom = True
 
+class MembershipInline(admin.TabularInline):
+    model = Postenart.disziplinarten.through
+    extra = 0  #Man kann mit einem JavaScript-Link neue dazufügen
+
+
+class BewertungsartInline(admin.TabularInline):
+    model = Bewertungsart
+    extra = 0  #Man kann mit einem JavaScript-Link neue dazufügen
+
+class DisziplinartAdmin(admin.ModelAdmin):
+    inlines = [
+            MembershipInline,
+            ]
+
+class PostenartAdmin(admin.ModelAdmin):
+    inlines = [
+            BewertungsartInline,
+            ]
+
 admin.site.register(Mitglied, MitgliedAdmin)
 admin.site.register(Sektion)
-admin.site.register(Postenart)
-admin.site.register(Bewertungsart)
+admin.site.register(Disziplinart, DisziplinartAdmin)
+admin.site.register(Postenart, PostenartAdmin)
