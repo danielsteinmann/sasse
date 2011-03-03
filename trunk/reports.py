f71eedfd5d85179587272b56d4980aaf94526051
dsteinmann@acm.org <dsteinmann@acm.org@19ca2394-1d08-11de-b6aa-3535bc3264a2>
2011-03-03 14:57:29 +0000

    Notenblatt PDF fertig.

--- trunk/reports.py
+++ trunk/reports.py
@@ -6,6 +6,7 @@ from reportlab.lib import colors
 from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.styles import StyleSheet1
 from reportlab.lib.units import cm
 from reportlab.platypus import BaseDocTemplate
 from reportlab.platypus import PageBreak
@@ -18,10 +19,14 @@ from reportlab.platypus.flowables import DocExec
 from reportlab.platypus.frames import Frame
 
 PAGE_WIDTH, PAGE_HEIGHT = A4
-LEFT = ParagraphStyle('left', fontName='Helvetica', fontSize=8, alignment=TA_LEFT)
-CENTER = ParagraphStyle('center', parent=LEFT, alignment=TA_CENTER)
-RIGHT = ParagraphStyle('right', parent=LEFT, alignment=TA_RIGHT)
-DS_MARKER = '<font size=-2> (DS)</font>'
+
+def _create_style_sheet(baseFontSize=8):
+    stylesheet = StyleSheet1()
+    stylesheet.add(ParagraphStyle(name='left', fontName='Helvetica',
+        fontSize=baseFontSize, alignment=TA_LEFT))
+    stylesheet.add(ParagraphStyle('center', parent=stylesheet['left'], alignment=TA_CENTER))
+    stylesheet.add(ParagraphStyle('right', parent=stylesheet['left'], alignment=TA_RIGHT))
+    return stylesheet
 
 def _extract_jahrgang(geburtstag):
     return ' <font size=-2>' + geburtstag.strftime('%Y') + "</font>"
@@ -35,25 +40,26 @@ def create_rangliste_doctemplate(wettkampf, disziplin):
     return doc
 
 def create_rangliste_flowables(rangliste, kategorie, kranzlimite):
-    result = []
     data = [['Rang', 'Steuermann', 'Vorderfahrer', 'Sektion', 'Stnr', 'Zeit', 'Punkte']]
     col_widths = (30, 140, 140, 80, 30, 40, 30)
     anzahl_ohne_kranz = 0
+    ds_marker = '<font size=-2> (DS)</font>'
+    ss = _create_style_sheet()
     for row in rangliste:
         if not row['kranz']: anzahl_ohne_kranz += 1
         if row['doppelstarter']: row['rang'] = 'DS'
-        if row['steuermann_ist_ds']: row['steuermann'] += DS_MARKER
-        if row['vorderfahrer_ist_ds']: row['vorderfahrer'] += DS_MARKER
+        if row['steuermann_ist_ds']: row['steuermann'] += ds_marker
+        if row['vorderfahrer_ist_ds']: row['vorderfahrer'] += ds_marker
         row['steuermann'] += _extract_jahrgang(row['steuermann_jg'])
         row['vorderfahrer'] += _extract_jahrgang(row['vorderfahrer_jg'])
         record = []
-        record.append(Paragraph(unicode(row['rang']), CENTER))
-        record.append(Paragraph(row['steuermann'], LEFT))
-        record.append(Paragraph(row['vorderfahrer'], LEFT))
-        record.append(Paragraph(row['sektion'], LEFT))
-        record.append(Paragraph(unicode(row['startnummer']), CENTER))
-        record.append(Paragraph(unicode(row['zeit_tot']), CENTER))
-        record.append(Paragraph(unicode(row['punkt_tot']), CENTER))
+        record.append(Paragraph(unicode(row['rang']), ss['center']))
+        record.append(Paragraph(row['steuermann'], ss['left']))
+        record.append(Paragraph(row['vorderfahrer'], ss['left']))
+        record.append(Paragraph(row['sektion'], ss['left']))
+        record.append(Paragraph(unicode(row['startnummer']), ss['center']))
+        record.append(Paragraph(unicode(row['zeit_tot']), ss['center']))
+        record.append(Paragraph(unicode(row['punkt_tot']), ss['center']))
         data.append(record)
     table_props = [
         ('LINEBELOW', (0,0), (-1,0), 1, colors.black),
@@ -68,11 +74,13 @@ def create_rangliste_flowables(rangliste, kategorie, kranzlimite):
         ]
     if anzahl_ohne_kranz:
         kranzlimite_row = len(data) - anzahl_ohne_kranz
-        data.insert(kranzlimite_row, [Paragraph('Kranzlimite: %s Punkte' % (kranzlimite), LEFT)])
+        text = Paragraph('Kranzlimite: %s Punkte' % (kranzlimite), ss['left'])
+        data.insert(kranzlimite_row, [text, None, None, None, None, None, None])
         table_props.extend([
             ('SPAN', (0,kranzlimite_row), (-1,kranzlimite_row)),
             ('BACKGROUND', (0,kranzlimite_row), (-1,kranzlimite_row), colors.lightgrey),
             ])
+    result = []
     result.append(DocExec("kategorie_name = '%s'" % kategorie.name))
     result.append(Platypus_Table(data, repeatRows=1, colWidths=col_widths, style=TableStyle(table_props)))
     result.append(PageBreak())
@@ -98,59 +106,68 @@ def write_rangliste_header_footer(canvas, doc):
 #
 
 def create_notenblatt_doctemplate(wettkampf, disziplin):
-    f = Frame(2.5*cm, 1*cm, PAGE_WIDTH-2*cm, PAGE_HEIGHT-2.5*cm, id='normal')
-    pt = PageTemplate(id="Notenblatt", frames=f)
+    f = Frame(2.5*cm, 1*cm, PAGE_WIDTH-2*cm, PAGE_HEIGHT-3*cm, id='normal')
+    pt = PageTemplate(id="Notenblatt", frames=f, onPage=write_notenblatt_header_footer)
     doc = BaseDocTemplate(None, pageTemplates=[pt], pagesize=A4)
+    doc.wettkampf = wettkampf
+    doc.disziplin = disziplin
     return doc
+
+def write_notenblatt_header_footer(canvas, doc):
+    canvas.saveState()
+    canvas.setFont('Helvetica-Bold', 10)
+    canvas.drawString(2.8*cm, PAGE_HEIGHT-1*cm, "Notenblatt %s" % (doc.disziplin.disziplinart))
+    canvas.drawRightString(PAGE_WIDTH-2*cm, PAGE_HEIGHT-1*cm, "%s %d" % (doc.wettkampf.name, doc.wettkampf.jahr()))
+    canvas.restoreState()
 
 def create_notenblatt_flowables(posten_werte, schiffeinzel):
     result = []
+    ss = _create_style_sheet(baseFontSize=10)
+    # --------
+    steuermann = schiffeinzel.steuermann
+    vorderfahrer = schiffeinzel.vorderfahrer
     data = [
-            ["Startnummer:", unicode(schiffeinzel.startnummer)],
-            ["Kategorie:", schiffeinzel.kategorie],
+            ["Steuermann:", "%s %s" % (steuermann.name, steuermann.vorname)],
+            ["Vorderfahrer:", "%s %s" % (vorderfahrer.name, vorderfahrer.vorname)],
             ["Sektion:", schiffeinzel.sektion],
-            ["Steuermann:", "%s %s" % (schiffeinzel.steuermann.name, schiffeinzel.steuermann.vorname)],
-            ["Vorderfahrer:", "%s %s" % (schiffeinzel.vorderfahrer.name, schiffeinzel.vorderfahrer.vorname)],
+            ["Kategorie:", schiffeinzel.kategorie],
+            ["Startnummer:", unicode(schiffeinzel.startnummer)],
             ]
-    col_widths = (70, 100)
+    col_widths = (70, 150)
     table_props = [
-        ('FONT', (0,0), (0,-1), 'Helvetica-Bold', 8),
+        ('FONT', (0,0), (-1,-1), 'Helvetica', 10),
+        ('FONT', (1,0), (1,-1), 'Helvetica-Bold', 10),
         ('TOPPADDING', (0,0), (-1,-1), 1),
         ('BOTTOMPADDING', (0,0), (-1,-1), 1),
         ]
     result.append(Platypus_Table(data, hAlign="LEFT", colWidths=col_widths, style=TableStyle(table_props)))
     result.append(Spacer(1, 30))
-    header = []
-    header.append(Paragraph('<strong>Posten</strong>', CENTER))
-    header.append(Paragraph('<strong>Übungsteil</strong>', LEFT))
-    header.append(Paragraph('<strong>Abzug</strong>', RIGHT))
-    header.append(Paragraph('<strong>Note</strong>', RIGHT))
-    header.append(Paragraph('<strong>Zeit</strong>', CENTER))
-    header.append(Paragraph('<strong>Total</strong>', RIGHT))
-    data = []
-    data.append(header)
-    col_widths = (40, 200, 30, 30, 40, 30)
+    # --------
+    data = [['Posten', 'Übungsteil', 'Abzug', 'Note', 'Zeit', 'Total',]]
+    col_widths = (40, 250, 40, 40, 55, 40)
     for row in posten_werte:
         record = []
         if row.get('posten'):
-            record.append(Paragraph(row['posten'], CENTER))
-            record.append(Paragraph(row['posten_art'], LEFT))
-            record.append(Paragraph(unicode(row['abzug']), RIGHT))
-            record.append(Paragraph(unicode(row['note']), RIGHT))
-            record.append(Paragraph(unicode(row['zeit']), CENTER))
-            record.append(Paragraph(unicode(row['total']), RIGHT))
+            record.append(Paragraph(row['posten'], ss['center']))
+            record.append(Paragraph(row['posten_art'], ss['left']))
+            record.append(unicode(row['abzug']))
+            record.append(unicode(row['note']))
+            record.append(unicode(row['zeit']))
+            record.append(unicode(row['total']))
         else:
-            record.append("")
-            record.append("")
-            record.append("")
-            record.append("")
-            record.append(Paragraph("<strong>" + unicode(row['zeit']) + "</strong>", CENTER))
-            record.append(Paragraph("<strong>" + unicode(row['total']) + "</strong>", RIGHT))
+            for i in range(0,4): record.append("")
+            record.append(unicode(row['zeit']))
+            record.append(unicode(row['total']))
         data.append(record)
     table_props = [
+        ('FONT', (0,0), (-1,-1), 'Helvetica', 10),
+        ('FONT', (0,0), (-1,0), 'Helvetica-Bold', 10),
+        ('FONT', (0,-1), (-1,-1), 'Helvetica-Bold', 10),
+        ('ALIGN', (0,0), (0,-1), 'CENTER'),
+        ('ALIGN', (-4,0), (-1,-1), 'RIGHT'),
         ('LINEBELOW', (0,0), (-1,0), 1, colors.black),
         ('LINEABOVE', (0,-1), (-1,-1), 1, colors.black),
-        ('ROWBACKGROUNDS', (0,1), (-1,-1), (None, colors.HexColor(0xf0f0f0))),
+        ('ROWBACKGROUNDS', (0,1), (-1,-2), (None, colors.HexColor(0xf0f0f0))),
         ]
     result.append(Platypus_Table(data, hAlign="LEFT", colWidths=col_widths, style=TableStyle(table_props)))
     return result
