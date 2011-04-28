e3e511b7f15409fbb36c530cb746263f9d6a2ec5
dsteinmann@acm.org <dsteinmann@acm.org@19ca2394-1d08-11de-b6aa-3535bc3264a2>
2011-04-28 17:03:13 +0000

    Test Wettkampf Creator verbessert

--- trunk/tests/random_wettkampf.py
+++ trunk/tests/random_wettkampf.py
@@ -15,23 +15,37 @@ from sasse.models import Kategorie
 from sasse.models import Bewertung
 from sasse.models import Bewertungsart
 from sasse.models import Teilnehmer
+from sasse.models import Richtzeit
 from sasse.forms import get_startkategorie, get_kategorie
 
 wettkampf_name = "My-Next-Test"
 for w in Wettkampf.objects.filter(name=wettkampf_name):
     w.delete()
+print "Alte Daten für %s gelöscht" % wettkampf_name
+
 wettkampf = Wettkampf.objects.create(name=wettkampf_name, zusatz="Bla", von=date.today())
 disziplin = Disziplin.objects.create(name="Einzelfahren", wettkampf=wettkampf)
 for k in Kategorie.objects.all():
     disziplin.kategorien.add(k)
-postenarten = Postenart.objects.filter(disziplinarten__disziplin=disziplin.disziplinart)
-for i, postenart in enumerate(postenarten):
+
+anzahl_normale_posten = 12
+postenarten = Postenart.objects.filter(disziplinarten__disziplin=disziplin.disziplinart).exclude(name="Zeitnote")
+for i in range(0, anzahl_normale_posten):
+    postenart = random.choice(postenarten)
+    name = chr(i+64)
     p = Posten.objects.create(name=chr(i+65), disziplin=disziplin, postenart=postenart, reihenfolge=i)
+
+anzahl_zeit_posten = 3
+zeitnote = Postenart.objects.get(name="Zeitnote", disziplinarten__disziplin=disziplin.disziplinart)
+for i in range(0, anzahl_zeit_posten):
+    p = Posten.objects.create(name="A-"+chr(i+65), disziplin=disziplin, postenart=zeitnote, reihenfolge=anzahl_normale_posten+1+i)
+    random_zeit = Decimal(random.uniform(40.0, 150.0))
+    richtzeit = Richtzeit.objects.create(posten=p, zeit=random_zeit)
 
 startnummer = 1
 for s in Sektion.objects.all():
     mitglieder = list(Mitglied.objects.filter(sektion=s).order_by('geburtsdatum'))
-    for i in xrange(0, len(mitglieder), 2):
+    for i in range(0, len(mitglieder), 2):
         hinten = mitglieder[i]
         if len(mitglieder) > i+1:
             vorne = mitglieder[i+1]
@@ -48,18 +62,27 @@ for s in Sektion.objects.all():
                     kategorie=kategorie)
             startnummer += 1
     print u"Startliste für %s erzeugt" % s
-schiffe = Teilnehmer.objects.filter(disziplin=disziplin)
+
+schiffe = list(Teilnehmer.objects.filter(disziplin=disziplin))
 for p in disziplin.posten_set.all():
+    richtzeit = None
+    if p.postenart.name == 'Zeitnote':
+        richtzeit = float(Richtzeit.objects.get(posten=p).zeit)
+        zeit_from = richtzeit - (richtzeit * 0.05)
+        zeit_until = richtzeit * 2
     for bart in Bewertungsart.objects.filter(postenart=p.postenart, editierbar=True):
         gueltige_werte_str = bart.wertebereich
         gueltige_werte = [Decimal(v.strip()) for v in gueltige_werte_str.split(',')]
         for s in schiffe:
             b = Bewertung(teilnehmer=s, posten=p, bewertungsart=bart)
-            if p.postenart.name == 'Zeitnote':
-                b.zeit = Decimal(random.randrange(20, 40, 1))
+            if richtzeit:
+                b.zeit = Decimal(random.uniform(zeit_from, zeit_until))
+                b.save()
             else:
-                b.note = Decimal(random.choice(gueltige_werte))
-            b.save()
+                random_note = Decimal(random.choice(gueltige_werte))
+                if random_note != bart.defaultwert:
+                    b.note = Decimal(random_note)
+                    b.save()
     print u"Werte für Posten %s erzeugt" % p
 
 
