1ef81755ea81e9fe4cbe0848ce8f6181b6333081
dsteinmann@acm.org <dsteinmann@acm.org@19ca2394-1d08-11de-b6aa-3535bc3264a2>
2011-05-19 21:34:57 +0000

    Selects vereinfacht und optimiert

--- sasse/templates/notenliste.sql
+++ sasse/templates/notenliste.sql
@@ -11,13 +11,13 @@ select tn.startnummer as Startnr
      , sum(case when b.posten_id = {{ p.id }} then b.zeit end) as "{{ p.name }} [s]"
 {% endifequal %}
 {% endfor %}
-  from bewertung_calc b
-       join sasse_teilnehmer tn on (tn.id = b.teilnehmer_id)
-       join sasse_schiffeinzel schiff on (schiff.teilnehmer_ptr_id = tn.id)
-       join sasse_kategorie kat on (kat.id = schiff.kategorie_id)
-       join sasse_sektion sektion on (sektion.id = schiff.sektion_id)
-       join sasse_mitglied vorne on (vorne.id = schiff.vorderfahrer_id)
-       join sasse_mitglied hinten on (hinten.id = schiff.steuermann_id)
+  from sasse_teilnehmer tn
+  join sasse_schiffeinzel schiff on (schiff.teilnehmer_ptr_id = tn.id)
+  join bewertung_calc b on (b.teilnehmer_id = tn.id)
+  join sasse_kategorie kat on (kat.id = schiff.kategorie_id)
+  join sasse_sektion sektion on (sektion.id = schiff.sektion_id)
+  join sasse_mitglied vorne on (vorne.id = schiff.vorderfahrer_id)
+  join sasse_mitglied hinten on (hinten.id = schiff.steuermann_id)
  where tn.disziplin_id = %s
        {% if sektion %}and schiff.sektion_id = %s{% endif %}
        {% if startnummern %}and tn.startnummer in (__startnummern__){% endif %}
--- sasse/templates/rangliste.sql
+++ sasse/templates/rangliste.sql
@@ -1,7 +1,7 @@
 select tn.startnummer as Startnr
-     , every(tn.ausgeschieden) as Ausgeschieden
-     , every(tn.disqualifiziert) as Disqualifiziert
-     , case when not every(tn.ausgeschieden) and not every(tn.disqualifiziert) and (
+     , tn.ausgeschieden as Ausgeschieden
+     , tn.disqualifiziert as Disqualifiziert
+     , case when not tn.ausgeschieden and not tn.disqualifiziert and (
           select kl.wert
             from sasse_kranzlimite kl
            where 1=1
@@ -14,7 +14,7 @@ select tn.startnummer as Startnr
                  join sasse_schiffeinzel schiff on (
                   schiff.vorderfahrer_id = m.id or schiff.steuermann_id = m.id)
                  join sasse_teilnehmer t on (t.id = schiff.teilnehmer_ptr_id)
-           where m.id = max(hinten.id) and t.disziplin_id = max(p.disziplin_id)
+           where m.id = max(hinten.id) and t.disziplin_id = max(tn.disziplin_id)
        ) = tn.startnummer then 0 else 1 end as SteuermannIstDS
      , case when (
           select min(t.startnummer) normale_startnummer
@@ -22,7 +22,7 @@ select tn.startnummer as Startnr
                  join sasse_schiffeinzel schiff on (
                   schiff.vorderfahrer_id = m.id or schiff.steuermann_id = m.id)
                  join sasse_teilnehmer t on (t.id = schiff.teilnehmer_ptr_id)
-           where m.id = max(vorne.id) and t.disziplin_id = max(p.disziplin_id)
+           where m.id = max(vorne.id) and t.disziplin_id = max(tn.disziplin_id)
        ) = tn.startnummer then 0 else 1 end as VorderfahrerIstDS
      , max(hinten.name) as Steuermann
      , max(hinten.vorname) as SteuermannVorname
@@ -34,15 +34,14 @@ select tn.startnummer as Startnr
      , max(kat.name) as Kategorie
      , sum(b.zeit) as Zeit
      , sum(case when tn.ausgeschieden or tn.disqualifiziert then 0 else b.note end) as Punkte
-  from bewertung_calc b
-  join sasse_teilnehmer tn on (tn.id = b.teilnehmer_id)
-  join sasse_posten p on (p.id = b.posten_id)
+  from sasse_teilnehmer tn
   join sasse_schiffeinzel schiff on (schiff.teilnehmer_ptr_id = tn.id)
+  join bewertung_calc b on (b.teilnehmer_id = tn.id)
   join sasse_kategorie kat on (kat.id = schiff.kategorie_id)
   join sasse_sektion sektion on (sektion.id = schiff.sektion_id)
   join sasse_mitglied vorne on (vorne.id = schiff.vorderfahrer_id)
   join sasse_mitglied hinten on (hinten.id = schiff.steuermann_id)
- where p.disziplin_id = %s
+ where tn.disziplin_id = %s
        {% if kategorie %}and kat.id = %s{% endif %}
- group by tn.startnummer
+ group by tn.startnummer, tn.ausgeschieden, tn.disqualifiziert
  order by Punkte desc, Zeit asc
