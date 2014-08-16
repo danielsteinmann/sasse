be049e19e636fbbbc254758c72a68e0cbaa576ba
steinmann.daniel@gmail.com <steinmann.daniel@gmail.com@19ca2394-1d08-11de-b6aa-3535bc3264a2>
2014-08-16 20:29:55 +0000

    Eingabe einer beliebigen Zahl ermöglichen

--- trunk/setup.py
+++ trunk/setup.py
@@ -24,7 +24,7 @@ class website_sdist(sdist):
 
 setup(
     name = "sasse",
-    version = "2.4",
+    version = "2.5",
     author = "Daniel Steinmann",
     author_email = "dsteinmann@acm.org",
     description = "Unterstützt das Rechnungsbüro der Pontoniere bei der Durchführung eines Wettkampfes.",
