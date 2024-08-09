f06184fd4fa9144f21ad15e137e204d8fd5cc142
Daniel Steinmann <daniel.steinmann@swisscom.com>
2024-08-09 14:53:34 +0200

    Release 3.1.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class website_sdist(sdist):
 
 setup(
     name = "sasse",
-    version = "3.0.0",
+    version = "3.1.0",
     author = "Daniel Steinmann",
     author_email = "dsteinmann@acm.org",
     description = "Unterstützt das Rechnungsbüro der Pontoniere bei der Durchführung eines Wettkampfes.",
